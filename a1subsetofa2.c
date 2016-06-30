e3a08ad8264d08e54c19b90114f52b3deff6b299
krithikaa <krithikam1995@gmail.com>
2016-06-30 16:03:14 +0530

    Create a1subsetofa2.c

---
+++ a1subsetofa2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void) {
+	//variable declaration
+	int n,m,i,j,flag=0;
+	//scanf("%d%d",&n,&m);
+	n=5,m=10;
+	int a1[n],a2[m];
+	
+	//array elements
+	for(i=0;i<m;i++)
+	  scanf("%d",&a2[i]);
+	for(i=0;i<n;i++)
+	  scanf("%d",&a1[i]);
+	  
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<m;j++)
+		{
+			if(a1[i]!=a2[j])
+			{
+				flag++;
+			}
+		}
+	}
+	if(flag==n)
+		printf("subset");
+	else
+		printf("not a subset");
+	
+	return 0;
+}
